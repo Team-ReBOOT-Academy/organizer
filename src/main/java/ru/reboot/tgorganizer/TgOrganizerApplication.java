b200595251a5cc39d93bc118f17fb7f20f31d151
Roman <romanplotskiy@gmail.com>
2026-04-12 22:02:09 +0600

    Понижена версия Java до 21 (Если требуется, установите), удалены лишние зависимости (можете подключать нужные в отдельных ветках, а то весь проект крашится с ними)

--- src/main/java/ru/reboot/tgorganizer/TgOrganizerApplication.java
+++ src/main/java/ru/reboot/tgorganizer/TgOrganizerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TgOrganizerApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(TgOrganizerApplication.class, args);
     }
 
