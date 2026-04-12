936b920a4b5e5a0259c54992219b62c30221799e
Roman <romanplotskiy@gmail.com>
2026-04-13 00:11:12 +0600

    Добавлен класс для автоматической проверки и выбора маршрутизациию. Реализована система попыток переподключения при запуске программы.

--- src/main/java/ru/reboot/tgorganizer/config/TelegramBotConfig.java
+++ src/main/java/ru/reboot/tgorganizer/config/TelegramBotConfig.java
@@ -1,6 +1,7 @@
 package ru.reboot.tgorganizer.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.Credentials;
 import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,30 +12,63 @@ import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.longpolling.util.TelegramOkHttpClientFactory;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
+import ru.reboot.tgorganizer.utils.TelegramConnectionTester;
 
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Configuration
 public class TelegramBotConfig {
     @Bean(value = "okClient")
     public OkHttpClient okHttpClient(
+            TelegramConnectionTester tester,
             @Value("${proxy.host}") String host,
             @Value("${proxy.port}") int port,
             @Value("${proxy.user}") String username,
             @Value("${proxy.pass}") String password
     ) {
-        return new TelegramOkHttpClientFactory.HttpProxyOkHttpClientCreator(
-                () -> new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, port)),
-                () -> (route, response) -> {
-                    String credentials = Credentials.basic(username, password);
-                    return response
-                            .request()
-                            .newBuilder()
-                            .header("Proxy-Authorization", credentials)
-                            .build();
-                }
-        ).get();
+        int[] backoffSeconds = {2, 5, 10, 15, 30};
+        int attempt = 0;
+
+        while (true) {
+            log.info("Инициализация HTTP-клиента для Telegram...");
+
+            if (tester.isDirectConnectionAvailable()) {
+                log.info("Будет использовано прямое подключение.");
+                return new OkHttpClient();
+            }
+
+            log.warn("Прямое соединение недоступно. Попытка использовать прокси...");
+            if (tester.isProxyConnectionAvailable(host, port, username, password)) {
+                log.info("Будет использовано подключение через прокси.");
+
+                return new TelegramOkHttpClientFactory.HttpProxyOkHttpClientCreator(
+                        () -> new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, port)),
+                        () -> (route, response) -> {
+                            String credentials = Credentials.basic(username, password);
+                            return response
+                                    .request()
+                                    .newBuilder()
+                                    .header("Proxy-Authorization", credentials)
+                                    .build();
+                        }
+                ).get();
+            }
+
+            int waitTime = (attempt < backoffSeconds.length) ? backoffSeconds[attempt] : 30;
+            log.error("Нет доступа к Telegram API. Повторная попытка через {} секунд", waitTime);
+
+            try {
+                TimeUnit.SECONDS.sleep(waitTime);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new IllegalStateException("Ожидание сети было прервано", e);
+            }
+
+            attempt++;
+        }
     }
 
     @Bean(value = "telegramClient")
