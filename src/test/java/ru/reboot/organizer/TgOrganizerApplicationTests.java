7cfa2cc53b7b54c612b088e15ebee0dd822b5735
Roman <romanplotskiy@gmail.com>
2026-05-11 18:12:58 +0600

    Архитектурные изменения в pom.xml, небольшие изменения в тестах

--- src/test/java/ru/reboot/organizer/TgOrganizerApplicationTests.java
+++ src/test/java/ru/reboot/organizer/TgOrganizerApplicationTests.java
@@ -1,10 +1,24 @@
 package ru.reboot.organizer;
 
+import okhttp3.OkHttpClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "bot.telegram.token=test_token",
+        "proxy.host=",
+        "proxy.port=0",
+        "proxy.user=",
+        "proxy.pass="
+})
 class TgOrganizerApplicationTests {
+    @MockitoBean(name = "okClient")
+    OkHttpClient okHttpClient;
+
+    @MockitoBean(name = "telegramBotsApplication")
+    TelegramBotsLongPollingApplication telegramBotsApplication;
 
     @Test
     void contextLoads() {
